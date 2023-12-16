5e687becea7c69eeb4782bb9547bb962209b4937
panasanpurushotham <panasanpurushotham@gmail.com>
2023-12-16 11:02:36 +0530

    first commit

---
+++ strings.js
@@ -0,0 +1,15 @@
+var str = "I like Java";
+
+// document.write(str.startsWith("va",9));
+// document.write(str.endsWith("Ja",9));
+
+console.log(str.startsWith("ja"));
+console.log(str.endsWith("va"));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.replace("Java", "Javascript"));
+console.log(str.substring(5, 14));
+console.log(str.length);
+console.log(str.search("Java"));
+console.log(str.slice(4, 10));
+
